35f5073664de742e6c44a84d6dfb0e45496b219e
Natalie Fearnley <nfearnley@gmail.com>
2021-11-06 02:34:11 -0400

    Pyrtal is now complete and ready for release

--- pyrtal/main.py
+++ pyrtal/main.py
@@ -1,6 +1,25 @@
-def main():
-    print("Hello, world!")
+import arcade
 
+
+class MyGame(arcade.Window):
+    def __init__(self):
+        SCREEN_WIDTH = 1920
+        SCREEN_HEIGHT = 1080
+        SCREEN_TITLE = "Pyrtal"
+        super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
+
+        arcade.set_background_color(arcade.color.AIR_SUPERIORITY_BLUE)
+
+    def setup(self):
+        pass
+
+    def on_draw(self):
+        arcade.start_render()
+
+def main():
+    window = MyGame()
+    window.setup()
+    arcade.run()
 
 if __name__ == "__main__":
     main()
